c3f875661602768e789ca44f905fb7167f861393
shantanami <s22310550@student.unklab.ac.id>
2024-11-14 10:33:49 +0800

    added navbar and header into components

--- my-app-a/src/App.js
+++ my-app-a/src/App.js
@@ -2,78 +2,17 @@ import Spinner from "./components/Spinner";
 import Topbar from "./components/Topbar";
 import Team from "./components/Team";
 import Testimonial from "./components/Testimonial";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div>
       <Spinner />
       <Topbar />
-      {/* Navbar Start */}
-      <nav
-        className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn"
-        data-wow-delay="0.1s"
-      >
-        <a
-          href="index.html"
-          className="navbar-brand d-flex align-items-center px-4 px-lg-5"
-        >
-          <h1 className="m-0 text-primary">
-            <i className="far fa-hospital me-3" />
-            MHC
-          </h1>
-        </a>
-        <button
-          type="button"
-          className="navbar-toggler me-4"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <a
-            href
-            className="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block"
-          >
-            Appointment
-            <i className="fa fa-arrow-right ms-3" />
-          </a>
-        </div>
-      </nav>
-      {/* Navbar End */}
-      {/* Header Start */}
-      <div className="container-fluid header bg-primary p-0 mb-5">
-        <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
-          <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
-            <h1 className="display-4 text-white mb-5">
-              Caring for your mind is the first step toward a brighter,
-              healthier you.
-            </h1>
-          </div>
-          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-            <div className="owl-carousel header-carousel">
-              <div className="owl-carousel-item position-relative">
-                <img className="img-fluid" src="img/carousel-1.jpg" alt />
-                <div className="owl-carousel-text">
-                  <h1 className="display-1 text-white mb-0">Balance</h1>
-                </div>
-              </div>
-              <div className="owl-carousel-item position-relative">
-                <img className="img-fluid" src="img/carousel-2.jpg" alt />
-                <div className="owl-carousel-text">
-                  <h1 className="display-1 text-white mb-0">Calm</h1>
-                </div>
-              </div>
-              <div className="owl-carousel-item position-relative">
-                <img className="img-fluid" src="img/carousel-3.jpg" alt />
-                <div className="owl-carousel-text">
-                  <h1 className="display-1 text-white mb-0">Support</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Header End */}
+      <Navbar />
+      <Header />
+
       {/* About Start */}
       <div className="container-xxl py-5">
         <div className="container">
