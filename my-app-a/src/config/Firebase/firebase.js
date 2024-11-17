d4354b927b9986774e907f696c090ceac71c3448
avri-el <parengkuanavriel@gmail.com>
2024-11-18 00:30:15 +0800

    add header firebase

--- my-app-a/src/config/Firebase/firebase.js
+++ my-app-a/src/config/Firebase/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,5 +16,6 @@ const firebaseConfig = {
     "https://frontendgroupproject-92614-default-rtdb.firebaseio.com/",
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
--- tes.js
+++ tes.js
@@ -1,30 +1,19 @@
-import { ref, onValue } from "firebase/database";
-import { useEffect, useState } from "react";
-import { database } from "../../config/Firebase/firebase"; // Sesuaikan jalur impor dengan struktur folder Anda
-
 const Header = () => {
-  const [header, setHeader] = useState({});
-
-  useEffect(() => {
-    const headerRef = ref(database, "header/"); // Gunakan instance database yang sudah diinisialisasi
-    onValue(headerRef, (snapshot) => {
-      const data = snapshot.val();
-      setHeader(data);
-    });
-  }, []);
-
   return (
     <div className="container-fluid header bg-primary p-0 mb-5">
       <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
         <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
-          <h1 className="display-4 text-white mb-5">{header.title}</h1>
+          <h1 className="display-4 text-white mb-5">
+            Caring for your mind is the first step toward a brighter, healthier
+            you.
+          </h1>
         </div>
         <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
           <div className="owl-carousel header-carousel">
             <div className="owl-carousel-item position-relative">
               <img className="img-fluid" src="img/carousel-2.jpg" alt />
               <div className="owl-carousel-text">
-                <h1 className="display-1 text-white mb-0">{header.h1img}</h1>
+                <h1 className="display-1 text-white mb-0">Calm</h1>
               </div>
             </div>
           </div>
