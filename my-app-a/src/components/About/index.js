cd0f11e87973e44137ee6033bc1278dd4f7e1fae
avri-el <parengkuanavriel@gmail.com>
2024-11-19 20:30:51 +0800

    add fade react di about

--- my-app-a/src/components/About/index.js
+++ my-app-a/src/components/About/index.js
@@ -1,6 +1,8 @@
 import { ref, onValue } from "firebase/database";
 import { useEffect, useState } from "react";
 import { database } from "../../config/Firebase/firebase";
+import React from "react";
+import { Fade } from "react-awesome-reveal";
 
 const About = () => {
   const [About, setAbout] = useState({});
@@ -21,50 +23,55 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container-xxl py-5">
-      <div className="container">
-        <div className="row g-5">
-          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-            <div className="d-flex flex-column">
-              <img
-                className="img-fluid rounded w-75 align-self-end"
-                src={`data:image/jpeg;base64, ${aboutImage.about1}`}
-                alt=""
-              />
-              <img
-                className="img-fluid rounded w-50 bg-white pt-3 pe-3"
-                src={`data:image/jpeg;base64, ${aboutImage.about2}`}
-                alt=""
-                style={{ marginTop: "-25%" }}
-              />
+    <Fade>
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-5">
+            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+              <div className="d-flex flex-column">
+                <img
+                  className="img-fluid rounded w-75 align-self-end"
+                  src={`data:image/jpeg;base64, ${aboutImage.about1}`}
+                  alt=""
+                />
+                <img
+                  className="img-fluid rounded w-50 bg-white pt-3 pe-3"
+                  src={`data:image/jpeg;base64, ${aboutImage.about2}`}
+                  alt=""
+                  style={{ marginTop: "-25%" }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-            <p className="d-inline-block border rounded-pill py-1 px-4">
-              About Us
-            </p>
-            <h1 className="mb-4">{About.title}</h1>
-            <p>{About.paragraph1}</p>
-            <p className="mb-4">{About.paragraph2}</p>
-            <p>
-              <i className="far fa-check-circle text-primary me-3" />
-              Quality health care
-            </p>
-            <p>
-              <i className="far fa-check-circle text-primary me-3" />
-              Only Qualified Doctors
-            </p>
-            <p>
-              <i className="far fa-check-circle text-primary me-3" />
-              Medical Research Professionals
-            </p>
-            <a className="btn btn-primary rounded-pill py-3 px-5 mt-3" href="#">
-              Read More
-            </a>
+            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+              <p className="d-inline-block border rounded-pill py-1 px-4">
+                About Us
+              </p>
+              <h1 className="mb-4">{About.title}</h1>
+              <p>{About.paragraph1}</p>
+              <p className="mb-4">{About.paragraph2}</p>
+              <p>
+                <i className="far fa-check-circle text-primary me-3" />
+                Quality health care
+              </p>
+              <p>
+                <i className="far fa-check-circle text-primary me-3" />
+                Only Qualified Doctors
+              </p>
+              <p>
+                <i className="far fa-check-circle text-primary me-3" />
+                Medical Research Professionals
+              </p>
+              <a
+                className="btn btn-primary rounded-pill py-3 px-5 mt-3"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
